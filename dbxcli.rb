306628f05c33b241fd86c8f656da818f2d5fccdc
Frank <FrankSansC@users.noreply.github.com>
2019-04-24 14:20:58 +0200

    Update dbxcli.rb

--- dbxcli.rb
+++ dbxcli.rb
@@ -1,9 +1,9 @@
 class Dbxcli < Formula
   desc "A command line client for Dropbox built using the Go SDK"
   homepage "https://github.com/dropbox/dbxcli"
-  url "https://github.com/dropbox/dbxcli/releases/download/v2.1.2/dbxcli-darwin-amd64"
-  version "2.1.2"
-  sha256 "8cce5799c70db66eaaaadab8568f341342a19d877243d67e658024c71a47b006"
+  url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-darwin-amd64"
+  version "3.0.0"
+  sha256 "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
 
   def install
     system "mv dbxcli-darwin-amd64 dbxcli"
