df59c69c1f634465da7116e4936adeba81be21d8
Frank <FrankSansC@users.noreply.github.com>
2019-05-21 08:23:28 +0200

    Use #{version} in URL

--- dbxcli.rb
+++ dbxcli.rb
@@ -1,8 +1,8 @@
 class Dbxcli < Formula
   desc "A command line client for Dropbox built using the Go SDK"
   homepage "https://github.com/dropbox/dbxcli"
-  url "https://github.com/dropbox/dbxcli/releases/download/v3.0.0/dbxcli-darwin-amd64"
   version "3.0.0"
+  url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-darwin-amd64"
   sha256 "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
 
   def install
